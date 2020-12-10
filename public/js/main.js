1f60146ad4c5493c1a07054789a25eb79aba4a13
Kat Archer <katrinaaarcher@hotmail.co.uk>
2020-12-10 12:51:05 +0000

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+console.log("Inside JavaScript front-end");
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const hbs = require('hbs');
+const axios = require('axios');
+
+//Finding the views folder directory
+const viewsPath = path.join(__dirname, '/views');
+
+//Setting Nodejs View Engine to use Handlebars(HBS) files
+app.set('view engine', 'hbs');
+
+//Setting the Views from HBS to come from our views path variable
+app.set('views', viewsPath);
+
+//Finding the public folder directory
+const publicDirectory = path.join(__dirname, '/public');
+
+//Setting express to use the static files from public Directory, files like CSS or JS
+app.use(express.static(publicDirectory));
+
+//Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({extended: false}));
+
+//Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    // console.log(req.query.city);
+    // console.log(req.query.country);
+    const cityName = "Nottingham";
+    const countryCode = "UK";
+    try {
+        const weather = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryCode}&units=metric&appid=b63099f2370ea84802dcf896425ed69e`);
+        console.log(weather.data);
+        res.render("index", {
+            temp: weather.data.main.temp,
+            feelsLike: weather.data.main["feels_like"],
+            weatherType: weather.data.weather[0].main,
+            place: weather.data.name
+        });
+    } catch (error) {
+        res.render("info");
+    }
+});
+
+app.get("/nottingham", (req, res) => {
+    res.render("nottingham");
+});
+
+//error 404 catcher. Must go after all others
+app.get("*", (req, res) => {
+    res.send("<h1>Sorry that page does not exist</h1>");
+})
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000")
+})
