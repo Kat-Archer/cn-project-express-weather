342a4ec723179e9534c383bfe34c2972c239aa44
Kat Archer <katrinaaarcher@hotmail.co.uk>
2020-12-11 11:47:12 +0000

    day 2 commit 1

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,34 @@
 console.log("Inside JavaScript front-end");
 
+let code = document.getElementById("date");
+console.log(code);
+code *= 1000;
+console.log(code);
+let date = new Date();
+console.log(date);
+date.toLocaleString();
+console.log(date);
+
+
+// const instruction = document.getElementById("instruction");instruction
+// const place = document.getElementById("place");
+// const temp = document.getElementById("temp");
+// const feels = document.getElementById("feels");
+// const type = document.getElementById("type");
+// const pic = document.getElementById("pic");
+
+// if (type === undefined) {
+//     instruction.style.visibility = "visible";
+//     place.style.visibility = "hidden";
+//     temp.style.visibility = "hidden";
+//     feels.style.visibility = "hidden";
+//     type.style.visibility = "hidden";
+//     pic.style.visibility = "hidden";
+// } else {
+//     instruction.style.visibility = "hidden";
+//     place.style.visibility = "visible";
+//     temp.style.visibility = "visible";
+//     feels.style.visibility = "visible";
+//     type.style.visibility = "visible";
+//     pic.style.visibility = "visible";
+// }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const hbs = require('hbs');
 const axios = require('axios');
+const { waitForDebugger } = require('inspector');
 const dotenv = require('dotenv').config();
 
 //Finding the views folder directory
@@ -26,15 +27,16 @@ app.use(express.urlencoded({extended: false}));
 //Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-    console.log(req.query.city);
-    console.log(req.query.country);
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.get("/displayWeather", async (req, res) => {
     const cityName = req.query.city;
     const countryCode = req.query.country;
     const url = process.env.APPID;
     try {
         const weather = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryCode}&units=metric&appid=${url}`);
-        console.log(weather.data);
         res.render("index", {
             temp: weather.data.main.temp,
             feelsLike: weather.data.main["feels_like"],
@@ -47,9 +49,42 @@ app.get("/", async (req, res) => {
     }
 });
 
-app.get("/nottingham", (req, res) => {
-    res.render("nottingham");
-});
+app.get("/nottingham", async (req, res) => {
+    const url = process.env.APPID;
+    console.log(url);
+    const forecast = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=52.9548&lon=-1.1581&units=metric&appid=${url}`);
+    console.log(forecast)
+    res.render("nottingham", {
+        fDate1: forecast.data.daily[1].dt,
+        fTemp1: forecast.data.daily[1].temp.day,
+        fWeatherType1: forecast.data.daily[1].weather[0].main,
+        fIcon1: forecast.data.daily[1].weather[0].icon,
+        fDate2: forecast.data.daily[2].dt,
+        fTemp2: forecast.data.daily[2].temp.day,
+        fWeatherType2: forecast.data.daily[2].weather[0].main,
+        fIcon2: forecast.data.daily[2].weather[0].icon,
+        fDate3: forecast.data.daily[3].dt,
+        fTemp3: forecast.data.daily[3].temp.day,
+        fWeatherType3: forecast.data.daily[3].weather[0].main,
+        fIcon3: forecast.data.daily[3].weather[0].icon,
+        fDate4: forecast.data.daily[4].dt,
+        fTemp4: forecast.data.daily[4].temp.day,
+        fWeatherType4: forecast.data.daily[4].weather[0].main,
+        fIcon4: forecast.data.daily[4].weather[0].icon,
+        fDate5: forecast.data.daily[5].dt,
+        fTemp5: forecast.data.daily[5].temp.day,
+        fWeatherType5: forecast.data.daily[5].weather[0].main,
+        fIcon5: forecast.data.daily[5].weather[0].icon,
+        fDate6: forecast.data.daily[6].dt,
+        fTemp6: forecast.data.daily[6].temp.day,
+        fWeatherType6: forecast.data.daily[6].weather[0].main,
+        fIcon6: forecast.data.daily[6].weather[0].icon,
+        fDate7: forecast.data.daily[7].dt,
+        fTemp7: forecast.data.daily[7].temp.day,
+        fWeatherType7: forecast.data.daily[7].weather[0].main,
+        fIcon7: forecast.data.daily[7].weather[0].icon,
+    });
+})
 
 //error 404 catcher. Must go after all others
 app.get("*", (req, res) => {
